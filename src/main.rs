9872015f75ce0e6e77b5c122165abf7206a359c8
abdul <abdul.emad0@gmail.com>
2025-01-02 21:44:59 +0000

    sharpen + width

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use clap::Parser;
-use image::{DynamicImage, GenericImageView, imageops::FilterType};
+use image::{DynamicImage, GenericImageView, imageops::FilterType, imageops};
 
 #[derive(Parser)]
 struct Cli {
     path: String,
-    width: u32
+    width: Option<u32>,
+    height: Option<u32>
 }
 
 fn main() {
     let cli = Cli::parse();
     let img = load_image(&cli.path);
-    let resized_img = resize_image(&img, cli.width);
-    print_ansi_image(&resized_img);
+    let resized_img = resize_image(&img, cli.width, cli.height);
+    let sharpened_img = sharpen_image(&resized_img);
+    print_ansi_image(&sharpened_img);
 }
 
 fn load_image(path: &str) -> DynamicImage{
@@ -21,11 +23,30 @@ fn load_image(path: &str) -> DynamicImage{
         .expect("Failed to decode image")
 }
 
-fn resize_image(img: &DynamicImage, width: u32) -> DynamicImage {
-    let (w,h) = img.dimensions();
-    let aspect_ratio = h as f64 / w as f64;
-    let new_height = (width as f64 * aspect_ratio) as u32;
-    img.resize_exact(width, new_height, FilterType::Triangle)
+fn resize_image(img: &DynamicImage, width: Option<u32>, height: Option<u32>) -> DynamicImage {
+    match (width, height) {
+        (Some(w), Some(h)) => {img.resize_exact(w, h, FilterType::Lanczos3)},
+        (Some(w), None) => {
+            let (orig_w, orig_h) = img.dimensions();
+            let aspect_ratio = orig_h as f64 / orig_w as f64;
+            let new_height = (w as f64 * aspect_ratio) as u32;
+            img.resize_exact(w, new_height, FilterType::Lanczos3)
+            },
+        (None, Some(h)) => {
+            let (orig_w, orig_h) = img.dimensions();
+            let aspect_ratio = orig_w as f64 / orig_h as f64;
+            let new_width = (h as f64 * aspect_ratio) as u32;
+            img.resize_exact(new_width, h, FilterType::Lanczos3)
+        }
+        (None, None) => img.clone(),
+        }
+}
+
+fn sharpen_image(img: &DynamicImage) -> DynamicImage {
+    let mut rgb = img.to_rgb8();
+
+    imageops::unsharpen(&mut rgb, 2.0, 1);
+    DynamicImage::ImageRgb8(rgb)
 }
 
 fn print_ansi_image(img: &DynamicImage){
